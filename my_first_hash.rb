cd1ba45dbdd670f21951ccc15ba893874aed168a
Shuo Wang <gken90@gmail.com>
2018-09-10 16:23:32 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -4,7 +4,12 @@ end
 
 
 def shipping_manifest
-  the_manifest = {"whale bone corsets" => 5, "poreclain vases" => 2, "oil paintings" => 3,}
+  the_manifest = {}
+  the_manifest["whale bone corsets"] = 5
+  the_manifest["poreclain vases"] =
+  the_manifest["oil paintings"] = 3
+
+
   # set a variable called `the_manifest`, equal to a hash
   # fill that hash with key/value pairs that describe the following information:
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings
